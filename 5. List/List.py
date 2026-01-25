c3c776d8cebc8dcd0d4b84c07e87b9a5697d8dfd
RusProgger <ruslantimka92@gmail.com>
2026-01-25 06:39:09 +0200

    Работа со списками в Python

--- 5. List/List.py
+++ 5. List/List.py
@@ -2,7 +2,7 @@
 
 cites = ["Kiev", "Odessa", "Ujgorod", "Poltava"]
 
-# Проверяем на тип данных
+# Проверяем какой из типов данных хранит переменная 
 print(type(cites))
 
 # Добавим елемент в конец списка
@@ -10,3 +10,11 @@ cites.append("Черкассы")
 
 # Вывод списка
 print(cites)
+
+# Расширяет список list, добавляя в конец все элементы списка 
+ext = ["Moskow", "Peter"]
+
+cites.extend(ext)
+
+print(cites)
+
