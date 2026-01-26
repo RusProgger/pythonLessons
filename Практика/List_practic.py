8c3b0722274b856f1699cb253a65bc628cf3bfb9
RusProgger <ruslantimka92@gmail.com>
2026-01-26 07:34:24 +0200

    Практика со списками в Питон

--- Практика/List_practic.py
+++ Практика/List_practic.py
@@ -5,6 +5,11 @@ color = []
 for i in range(4):
     color_input = input(f"Введите цвет {i + 1}: ")
     color.append(color_input)
+
+print("---------------------------")
+print(color)
+
+print("---------------------------")
 print(color[0])
 print(color[1])
 print(color[2])
