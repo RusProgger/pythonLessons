878f6bc37a7f9b36c51d5fc7247cf0dcd3cc136d
RusProgger <ruslantimka92@gmail.com>
2026-01-26 07:15:41 +0200

    Работа со списками в Питон

--- 5. List/List.py
+++ 5. List/List.py
@@ -72,4 +72,9 @@ print(names.count("L"))
 
 numbers_int = [1, 5, 10, 17, 88, 3, 12, 8, 3]
 
-# 
+# проверка существует ли в списке, 5
+
+if 4 in numbers_int:
+    print("В этом списке 5 существует")
+else:
+    print("Нет")
