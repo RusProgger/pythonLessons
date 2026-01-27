da2e52a50e183eeae43003a00a3e467859d4b327
RusProgger <ruslantimka92@gmail.com>
2026-01-27 07:14:59 +0200

    Работа со списками, сортировка

--- 5. List/List.py
+++ 5. List/List.py
@@ -91,6 +91,6 @@ number_num = [10, 6, 5, 1, 9, 3, 2, 7, 4, 0]
 # Сортируем список, но оригинал сохраняем
 print(f"Оригинальный список несортированный: {number_num}")
 number_sort = sorted(number_num)
-print(f"Отсортированный список через sorted: {number_num}")
+print(f"Отсортированный список через sorted: {number_sort}")
 
 
