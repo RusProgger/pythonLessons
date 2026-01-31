ad7e572747e73d2c62370f5a1890f7c89f4c2090
RusProgger <ruslantimka92@gmail.com>
2026-01-31 14:17:11 +0200

    Практика методы строк

--- Практика/String_method_practic.py
+++ Практика/String_method_practic.py
@@ -3,14 +3,14 @@
 
 number_count = int(input("Введите число от 1 до 5: "))
 
-if number_count > 6:
+if number_count < 1 or number_count > 5:
     print("Число превышает лимит...")
 else:
 
     for i in range(number_count):
         name = input("Введите имя: ")
 
-        if name != "":
+        if name.strip():
             print(f"Привет, {name.strip().upper()}")
         else:
             print("Имя не может быть пустым...")
