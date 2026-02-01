1c6747f6d93832aa5f0fd1e4dbca159f224f125a
RusProgger <ruslantimka92@gmail.com>
2026-02-01 20:04:32 +0200

    Практика с методами строк

--- Практика/String_method_practic.py
+++ Практика/String_method_practic.py
@@ -8,10 +8,10 @@ if number_count < 1 or number_count > 5:
 else:
 
     for i in range(number_count):
-        name = input("Введите имя: ")
+        name = input("Введите имя: ").strip()
 
         if name.strip():
-            print(f"Привет, {name.strip().upper()}")
+            print(f"Привет, {name.strip()}")
         else:
             print("Имя не может быть пустым...")
 
