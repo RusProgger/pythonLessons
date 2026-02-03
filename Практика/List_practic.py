0f381e326e8fc79ee670993c75840de13e11badb
RusProgger <ruslantimka92@gmail.com>
2026-02-03 19:30:45 +0200

    Практика с списками

--- Практика/List_practic.py
+++ Практика/List_practic.py
@@ -10,10 +10,11 @@ print("---------------------------")
 print(color)
 
 print("---------------------------")
+
 #print(color[0])
 #print(color[1])
 #print(color[2])
 #print(color[3])
 
 for i in range(4):
-    print(f"Вывод цвета {i}:  {color[i]}")
+    print(f"Вывод цвета {i + 1}:  {color[i]}")
