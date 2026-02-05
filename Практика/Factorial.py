5d112660a02db6c0a21813fe143dc22cdeaa8558
RusProgger <ruslantimka92@gmail.com>
2026-02-06 01:50:33 +0200

    Практика факториал числа

--- Практика/Factorial.py
+++ Практика/Factorial.py
@@ -1,5 +1,17 @@
 # Факториал числа
 
+def Fact(f):
+    f_result = 1
 
+    for i in range(1, f + 1):
+         f_result *= i
+         
+    return f_result
+
+
+number = int(input("Введите число: "))
+result = Fact(number)
+
+print(f"Факториал числа: {result}")
 
     
