3ab61eaf0b52ed2f51b9d04253a3eb23137ff95b
RusProgger <ruslantimka92@gmail.com>
2026-07-20 19:21:52 +0300

    Добавлен вывод длинного текста

--- 3. FormatInput/inputFormat.py
+++ 3. FormatInput/inputFormat.py
@@ -14,3 +14,5 @@ print(f"Вывод через f-строки: \nПривет, {name}")
 # print c параметрами sep и end
 
 print("a", "b", "c", sep=" | ", end="!\n")
+
+print("""Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n Lorem Ipsum has been the industry's standard dummy text ever since 1966, when designers at Letraset and James Mosley, the librarian at St Bride Printing Library in London, took a 1914 Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.\n It has survived not only many decades, but also the leap into electronic typesetting, remaining essentially unchanged.\n It was popularised thanks to these sheets and more recently with desktop publishing software like Aldus PageMaker and Microsoft Word including versions of Lorem Ipsum.""", end="!!!\n")
